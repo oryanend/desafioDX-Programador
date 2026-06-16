f158e4356ab1ecbda870e5962019848091370462
oryanend <ryanyamim@gmail.com>
2026-06-15 21:54:45 -0300

    feat: criar `Dockerfile` e `docker-compose.yml`

--- src/main/java/br/com/duxusdesafio/config/ResourceServerConfig.java
+++ src/main/java/br/com/duxusdesafio/config/ResourceServerConfig.java
@@ -25,5 +25,14 @@ public class ResourceServerConfig {
         return http.build();
     }
 
+    @Bean
+    @Order(2)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll());
 
+        return http.build();
+    }
 }
